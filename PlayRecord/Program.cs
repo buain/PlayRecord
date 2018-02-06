da829eb66ce320930ecd879139a5dd880f051c48
Vladimir <33662107+buain@users.noreply.github.com>
2018-02-06 23:47:19 +0300

    Initial commit

---
+++ PlayRecord/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPlayable player = new Player();
+            IRecodable recodable = new Player();
+            recodable.Record();
+            recodable.Pause();
+            recodable.Stop();
+            player.Play();
+            player.Pause();
+            player.Stop();
+
+            //Delay
+            Console.ReadKey();
+        }
+    }
+    interface IPlayable
+    {
+        void Play(); 
+        void Pause();
+        void Stop();
+    }
+    interface IRecodable
+    {
+        void Record();
+        void Pause();
+        void Stop();
+    }
+    class Player : IPlayable , IRecodable
+    {
+        public void Pause()
+        {
+            Console.WriteLine("Песня на паузе");
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("Песня играет");
+        }
+
+        public void Record()
+        {
+            Console.WriteLine("Запись песни");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Остановка песни");
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,6 @@
+Задание 3 
+Используя Visual Studio, создайте проект по шаблону Console Application.  
+Требуется:  
+Создайте 2 интерфейса IPlayable и IRecodable. В каждом из интерфейсов создайте по 3 метода void Play() / void Pause() / void Stop() и void Record() / void Pause() / void Stop() соответственно. 
+Создайте производный класс Player от базовых интерфейсов IPlayable и IRecodable. 
+Написать программу, которая выполняет проигрывание и запись. 
